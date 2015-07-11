f5c5991cfe4f84cde992e39f6033d7e8e09e8ba8
Beyta <sir.beyta@gmail.com>
2015-07-12 00:47:33 +0800

    1st Upload
    This is the first upload

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Wordpress Skeleton By Beyta</title>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
+</head>
+
+<body>
+<div id="wrapper">
+	<div id="header">
+		<h1>header</h1>
+   	</div>
